4360dd363550b5e3920fed73a09c47daac3ba9e2
root <root@localhost.localdomain>
2018-04-30 11:47:15 +0700

    First

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gudnai.wsgi --log-file -
---
+++ gudnight/tasks.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, unicode_literals
+from celery import task
+import requests
+import lxml.html
+
+@task()
+def send_message():
+
+    access_token = ''
+    appid = ''
+    s = requests.Session()
+    r = s.get('https://api.facebook.com/method/auth.getSessionforApp?access_token=' + access_token +'&format=json&new_app_id=' + appid +'&generate_session_cookies=1')
+    cookies = {}
+    for key in r.json()['session_cookies']:
+        cookies.update({key['name']: key['value']})
+    my_id = ""
+    id_send = ""
+    message = "Ngu ngonnnnnnnnnnn"
+    r = s.get('https://m.facebook.com/messages/read/?tid=cid.c.'+ id_send +':' + my_id + "&refid=11#fua", cookies=cookies)
+    tree = lxml.html.fromstring(r.content)
+
+    fb_dtsg = tree.xpath('//input[@name="fb_dtsg"]/@value')[0]
+    csid = tree.xpath('//input[@name="csid"]/@value')[0]
+    payload = "fb_dtsg=" + fb_dtsg +"&body=" + message + "&send=G%E1%BB%ADi&tids=cid.c." + id_send + ":" + my_id +"&wwwupp=C3&ids[" + id_send +"]:" + id_send + "&referrer=&ctype=&cver=legacy&csid=" + csid
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    r = s.post('https://m.facebook.com/messages/send/?icm=1&refid=12', data=payload, cookies=cookies, headers=headers)
